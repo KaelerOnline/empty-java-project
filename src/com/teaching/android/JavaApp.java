f0050b1e3fdbef9e897570a11840b04785c566e6
Equipo10\EOI <kaeleronlinefartoomuch@gmail.com>
2018-04-17 13:36:52 +0200

    Commit Nº2 Añadido retraso al imprimir los valores.

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -5,25 +5,30 @@ package com.teaching.android;
  */
 public class JavaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Práctica tipos primitivos"); // Display the string.
-        byte b= 65;         //8bits
-        char c= 'A';        //16bits
-        short s= 65;        //16bits
-        int i= 65;          //32bits
-        long l= 65L;        //64bits
-        float f= 65f;       //32bits
-        double d= 65.55;    //64bits
-        boolean bol= true;  //1bit
-        
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(s);
-        System.out.println(i);
-        System.out.println(l);
-        System.out.println(f);
-        System.out.println(d);
-        System.out.println(bol);
+        public static void main(String[] args) {
+            System.out.println("Práctica tipos primitivos"); // Display the string.
+            byte b= 65;         //8bits
+            char c= 'A';        //16bits
+            short s= 65;        //16bits
+            int i= 65;          //32bits
+            long l= 65L;        //64bits
+            float f= 65f;       //32bits
+            double d= 65.55;    //64bits
+            boolean bol= true;  //1bit
+            try {
+                Thread.sleep(5000);
+                System.out.println(b);
+                System.out.println(c);
+                System.out.println(s);
+                System.out.println(i);
+                System.out.println(l);
+                System.out.println(f);
+                System.out.println(d);
+                System.out.println(bol);
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
     }
 
 }
